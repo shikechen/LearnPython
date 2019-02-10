8987336316e3923ebb187098ae196affb086fd69
shikechen <806335962@qq.com>
2019-02-10 23:49:49 +0800

    Learn random

---
+++ 07_random/random_v1.0.py
@@ -0,0 +1,15 @@
+"""
+    Author: shikechen
+    Function: learn random
+    Version: 1.0
+    Date: 2019/2/10
+"""
+import random
+
+
+def main():
+    print('random value: {}'.format(random.random()))
+
+
+if __name__ == '__main__':
+    main()
