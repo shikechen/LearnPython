5671212c333a03e3afaa789a2a4bbc4ff77537ec
shikechen <806335962@qq.com>
2019-03-11 16:41:47 +0800

    Add iterate sheets to count data

--- 11_meal_report_statistical_system/meal_report_statistical_system.py
+++ 11_meal_report_statistical_system/meal_report_statistical_system.py
@@ -1,56 +1,58 @@
 """
     Author: shikechen
     Function: Company staff meal report intelligent statistical system
-    Version: 1.0
-    Date: 2019/3/8
+    Version: 1.1
+    Date: 2019/3/11
 """
 import openpyxl
 
 
 def main():
-    excel_name = '201902.xlsx'
+    excel_name = '2019_2.xlsx'
     book = openpyxl.load_workbook(excel_name)
     sheet_name_list = book.sheetnames
     print(sheet_name_list)
 
-    sheet = book.active
-    print(sheet.title)
+    for sheet_name in sheet_name_list:
+        print('sheet name: ', sheet_name)
+        if sheet_name.__contains__('-'):
+            sheet = book[sheet_name]
+            print('sheet title: ', sheet.title)
 
-    rows = sheet.rows
-    # columns = sheet.columns
-    # print(rows)
-    # print(columns)
+            rows = sheet.rows
 
-    sheet_max_row = sheet.max_row
-    print('sheet_max_row', sheet_max_row)
-    sheet_max_column = sheet.max_column
-    print('sheet_max_column', sheet_max_column)
+            sheet_max_row = sheet.max_row
+            print('sheet_max_row', sheet_max_row)
+            sheet_max_column = sheet.max_column
+            print('sheet_max_column', sheet_max_column)
 
-    row_index = 0
-    column_write_index = sheet_max_column + 1
-    for row in rows:
-        row_sum = 0
-        row_index += 1
+            row_index = 0
+            column_write_index = sheet_max_column + 1
+            for row in rows:
+                row_sum = 0
+                row_index += 1
 
-        for cell in row:
-            cell_value = cell.value
-            # print(cell_value)
+                # the first row is title
+                if row_index > 1:
+                    for cell in row:
+                        cell_value = cell.value
+                        # print(cell_value)
 
-            cell_sum = 0
+                        cell_sum = 0
 
-            if isinstance(cell_value, str):
-                if '早餐' in cell_value:
-                    cell_sum += 5
+                        if isinstance(cell_value, str):
+                            if '早餐' in cell_value:
+                                cell_sum += 5
 
-                if '午餐' in cell_value:
-                    cell_sum += 15
+                            if '午餐' in cell_value:
+                                cell_sum += 15
 
-                if '晚餐' in cell_value:
-                    cell_sum += 10
+                            if '晚餐' in cell_value:
+                                cell_sum += 10
 
-            row_sum += cell_sum
+                        row_sum += cell_sum
 
-        sheet.cell(row=row_index, column=column_write_index).value = row_sum
+                    sheet.cell(row=row_index, column=column_write_index).value = row_sum
 
     book.save(excel_name)
     book.close()
