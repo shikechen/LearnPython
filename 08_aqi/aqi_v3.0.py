929b93cd3e49c5756a71be771617bb747ce790b3
shikechen <806335962@qq.com>
2019-05-15 17:46:17 +0800

    Read Json file data and save to csv file

--- 08_aqi/aqi_v3.0.py
+++ 08_aqi/aqi_v3.0.py
@@ -1,10 +1,11 @@
 """
     Author: shikechen
-    Function: Read Json file data and rank data and save to Json file.
-    Version: 2.0
+    Function: Read Json file data and save to .csv file.
+    Version: 3.0
     Date: 2019/5/15
 """
 import json
+import csv
 
 
 def process_json_file(file_path):
@@ -17,10 +18,16 @@ def main():
     file_path = input('Please input Json file:')
     city_list = process_json_file(file_path)
     city_list.sort(key=lambda city: city['aqi'])
-    top5_list = city_list[:5]
 
-    f = open('top5_aqi.json', mode='w', encoding='utf-8')
-    json.dump(top5_list, f, ensure_ascii=False)
+    lines = []
+    lines.append(list(city_list[0].keys))
+    for city in city_list:
+        lines.append(list(city.values()))
+
+    f = open('aqi.csv', 'w', encoding='utf-8', newline='')
+    writer = csv.writer(f)
+    for line in lines:
+        writer.writerow(line)
     f.close()
 
 
